2e3e2a3c11971a948b90d12441f52447a1a5177a
William Silva <67198357+swilliamsilva@users.noreply.github.com>
2024-11-30 05:18:56 -0300

    Reorganizada a estrutura do diretório

---
+++ src/test/java/com/sistema/bancario/SistemaBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
